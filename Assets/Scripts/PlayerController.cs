cb2908fbfa162adfd0cadc21f0d7827a8a986759
That-Cat-Vivo <eman3001@yahoo.com>
2025-09-19 15:30:49 -0400

    Update PlayerController

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    //Body Reference
+    private Rigidbody rb;
+
+    //Character Coordinates
+    public float playerY;
+    public float playerX;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent;
     }
 
     // Update is called once per frame
