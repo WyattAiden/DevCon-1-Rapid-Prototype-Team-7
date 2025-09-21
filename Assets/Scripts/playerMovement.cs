a4e9e9957ef190fa958d5042e7a7a504b7e13828
That-Cat-Vivo <eman3001@yahoo.com>
2025-09-20 20:12:56 -0400

    edit ethanDevSceneMovement update playerMovement script with ability to move left and right.

--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class playerMovement : MonoBehaviour
 {
     private Rigidbody rb;
+    //Player move speed
+    public float moveSpeed = 10;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -12,8 +15,17 @@ public class playerMovement : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.MovePosition(transform.position + Vector3.left * moveSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.MovePosition(transform.position + Vector3.right * moveSpeed * Time.deltaTime);
+        }
     }
 }
